70d70914bce71373a45779ddeaa991529281f49e
serpent-charmer <zagirov.robert@yandex.ru>
2021-03-27 08:13:12 -0400

    Edited schemacrawler.sh to be be usable outside of distribution folder and as a symlink

--- schemacrawler-distrib/src/assembly/schemacrawler.sh
+++ schemacrawler-distrib/src/assembly/schemacrawler.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-SC_DIR=$(dirname "$0")
+is_symlink=$(dirname "${0}")
+if [[ $is_symlink == "." ]]; then
+	SC_DIR=$(dirname "$0")
+else
+	SC_DIR=$(dirname $(readlink "${0}"))
+fi
 java -cp "$SC_DIR"/lib/*:"$SC_DIR"/config schemacrawler.Main "$@"
